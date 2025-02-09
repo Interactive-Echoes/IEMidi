319ad1a56df721ed812cbacef09e0bcd69c51a21
mozahzah <mozahzah@gmail.com>
2025-02-09 15:11:30 -0500

    update to submodule, notifying OS on minimize

--- Source/IEMidi.cpp
+++ Source/IEMidi.cpp
@@ -120,6 +120,7 @@ void IEMidi::DrawMidiDeviceSelectionWindow()
                     else
                     {
                         Renderer.MinimizeAppWindow();
+                        Renderer.NotifyOSRunInBackground();
                     }
                 }
             }
